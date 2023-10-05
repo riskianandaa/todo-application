526062a291ee3f3c62cf65900777e63d62d332ff
riskianandaa <Risananda999@gmail.com>
2023-10-05 17:19:33 +0700

    feat: add theme

---
+++ src/components/button.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@/utils/theme'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <Box bg='primary' p='4' borderRadius='rounded-3xl' >
+      <Text color='white'>Button Component On Update</Text>
+    </Box>
+  )
+}
+
+export default Button
